78388ced1c1bfb58e0ad35d9eb3ada3bc081b6c4
InSungChang <changtecnologia@gmail.com>
2024-07-26 11:05:23 -0300

    Terminada galeria

--- public/js/galeria.js
+++ public/js/galeria.js
@@ -5,31 +5,32 @@ document.addEventListener('DOMContentLoaded', () => {
             renderData(data);
         })
         .catch(error => console.error('Erro ao carregar os dados:', error));
-        function renderData(data) {
-            const mediaGallery = document.getElementById('media-gallery');
-            mediaGallery.innerHTML = ''; // Limpa a galeria antes de renderizar novos dados
-    
-            data.forEach(item => {
-                const mediaItem = document.createElement('div');
-                mediaItem.classList.add('media-item');
-    
-                if (item.type === 'photo') {
-                    const img = document.createElement('img');
-                    img.src = `/uploads/${item.file_path}`;
-                    img.alt = item.caption;
-                    mediaItem.appendChild(img);
-                } else if (item.type === 'video') {
-                    const video = document.createElement('video');
-                    video.src = `/uploads/${item.file_path}`;
-                    video.controls = true;
-                    mediaItem.appendChild(video);
-                }
-    
-                const caption = document.createElement('p');
-                caption.textContent = item.caption;
-                mediaItem.appendChild(caption);
-    
-                mediaGallery.appendChild(mediaItem);
-            });
-        }
+
+    function renderData(data) {
+        const mediaGallery = document.getElementById('media-gallery');
+        mediaGallery.innerHTML = ''; // Limpa a galeria antes de renderizar novos dados
+
+        data.forEach(item => {
+            const mediaItem = document.createElement('div');
+            mediaItem.classList.add('media-item');
+
+            if (item.type === 'photo') {
+                const img = document.createElement('img');
+                img.src = `/uploads/${item.file_path}`;
+                img.alt = item.caption;
+                mediaItem.appendChild(img);
+            } else if (item.type === 'video') {
+                const video = document.createElement('video');
+                video.src = `/uploads/${item.file_path}`;
+                video.controls = true;
+                mediaItem.appendChild(video);
+            }
+
+            const caption = document.createElement('p');
+            caption.textContent = item.caption;
+            mediaItem.appendChild(caption);
+
+            mediaGallery.appendChild(mediaItem);
+        });
+    }
 });
